7e23cc3cf5fdb62412f13183ecdab4823f7ba826
Scott Stamp <scottstamp851@gmail.com>
2021-09-30 07:44:53 -0230

    initial work on standalone extractor

---
+++ AssetExtractor/AssetItem.cs
@@ -0,0 +1,29 @@
+﻿using AssetStudio;
+
+namespace AssetExtractor
+{
+    internal class AssetItem
+    {
+        public AssetStudio.Object Asset;
+        public SerializedFile SourceFile;
+        public string Container = string.Empty;
+        public string TypeString;
+        public long m_PathID;
+        public long FullSize;
+        public ClassIDType Type;
+        public string Text;
+        public string InfoText;
+        public string UniqueID;
+
+        public AssetItem(AssetStudio.Object asset, string path)
+        {
+            Asset = asset;
+            SourceFile = asset.assetsFile;
+            Type = asset.type;
+            TypeString = Type.ToString();
+            m_PathID = asset.m_PathID;
+            FullSize = asset.byteSize;
+            Text = path.Split('/').Last();
+        }
+    }
+}
---
+++ AssetExtractor/Program.cs
@@ -0,0 +1,32 @@
+﻿using AssetStudio;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetExtractor
+{
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var manager = new AssetsManager();
+            manager.LoadFolder(@"Z:\unity\Content");
+
+            foreach (var file in manager.assetsFileList)
+            {
+                var bundle = (AssetBundle)file.Objects.First(o => o.GetType() == typeof(AssetBundle));
+                foreach (var (path, asset) in bundle.m_Container)
+                {
+                    var assetObject = file.Objects.First(o => o.m_PathID == asset.asset.m_PathID);
+                    var fileName = path.Split('/').Last().Split('.').First();
+                    var filePath = string.Join('/', path.Split('/')[..^1]);
+
+                    Exporter.ExportConvertFile(new AssetItem(assetObject, fileName), "Z:/proper/" + filePath);
+                }
+            }
+        }
+    }
+}
